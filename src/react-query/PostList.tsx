742da04efa8d76536223c4ea9c955a62021a760b
kervin <kdimaculangan.dev@gmail.com>
2024-11-22 11:35:53 +0800

    Paginated queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,34 +1,38 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-  userId: number;
-}
+import { useState } from "react";
+import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [error, setError] = useState('');
+  const [page, setPage] = useState<number>(1);
+  const pageSize = 10;
+  const { data, error, isLoading } = usePosts({ page, pageSize });
 
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => setPosts(res.data))
-      .catch((error) => setError(error));
-  }, []);
-
-  if (error) return <p>{error}</p>;
-
+  if (error) return <p>{error.message}</p>;
+  if (isLoading) return <p>Loading...</p>;
   return (
-    <ul className="list-group">
-      {posts.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="list-group">
+        {data?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="my-2">
+        <button
+          className="btn btn-primary"
+          disabled={page === 1}
+          onClick={() => setPage(page - 1)}
+        >
+          Previous
+        </button>
+        <button
+          className="btn btn-primary ms-1"
+          onClick={() => setPage(page + 1)}
+        >
+          Next
+        </button>
+      </div>
+    </>
   );
 };
 
---
+++ src/react-query/hooks/usePosts.ts
@@ -0,0 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+interface PostQuery {
+  page: number;
+  pageSize: number;
+}
+
+const usePosts = (query: PostQuery) => {
+  return useQuery<Post[], Error>({
+    queryKey: ["posts", query],
+    queryFn: () =>
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts", {
+          params: {
+            _start: (query.page - 1) * query.pageSize,
+            _limit: query.pageSize,
+          },
+        })
+        .then((res) => res.data),
+    staleTime: 60 * 1000, //60s
+    keepPreviousData: true,
+  });
+};
+
+export default usePosts;
